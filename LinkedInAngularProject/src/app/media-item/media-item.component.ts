805eb5b1e80aae3ee89ce6e1c1fa7ec48ec83654
Teju-mestry <tejashreemestry40@gmail.com>
2020-06-28 02:50:08 +0530

    interpolation and expression context

---
+++ LinkedInAngularProject/src/app/media-item/media-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-media-item',
+  templateUrl: './media-item.component.html',
+  styleUrls: ['./media-item.component.css']
+})
+export class MediaItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  name= ' The Redemption';
+
+  wasWatch(){
+    return true;
+  }
+
+}
